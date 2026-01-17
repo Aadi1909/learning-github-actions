f5f71b03972bda76f7602ede1836dd62f40f3641
Aadi1909 <aadarshtyagi124@gmail.com>
2026-01-17 17:54:40 +0530

    learning github actions

---
+++ src/main/java/com/example/reactive/controller/HelloActions.java
@@ -0,0 +1,15 @@
+package com.example.reactive.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloActions {
+    
+
+    @GetMapping("/")
+    public String helloActions() {
+        return "Hello, Actions";
+    }
+
+}
