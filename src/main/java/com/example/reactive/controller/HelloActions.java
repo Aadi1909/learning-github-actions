b1482a2be0b5096054151da58da9ddc68c7b3a62
Aadi1909 <aadarshtyagi124@gmail.com>
2026-01-17 19:21:11 +0530

    changed dockerfile

--- src/main/java/com/example/reactive/controller/HelloActions.java
+++ src/main/java/com/example/reactive/controller/HelloActions.java
@@ -3,12 +3,16 @@ package com.example.reactive.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class HelloActions {
     
 
     @GetMapping("/hello")
     public String hello() {
+        log.info("Hello endpoint called");
         return "Hello, Github Actions!!";
     }
 
